bc802fc20293e1575d80ec3bff4b9d285e3c69a0
MuhammedMahmoud0 <muhammedmahmoud091@gmail.com>
2025-11-17 22:17:44 +0200

    the last thing inshaallah

--- app/app.py
+++ app/app.py
@@ -3,26 +3,49 @@ from pydantic import BaseModel
 import pandas as pd
 import joblib
 
-model = joblib.load("../models/model.pkl")
+# Load your trained pipeline (including preprocessing)
+pipeline = joblib.load("../models/model.pkl")
 
 app = FastAPI(title="Employee Attrition Prediction")
 
+
 class EmployeeData(BaseModel):
-    Age: int
-    MonthlyIncome: float
-    JobSatisfaction: int
+    employee_id: int
+    age: int
+    gender: str
+    years_at_company: int
+    job_role: str
+    monthly_income: float
+    work_life_balance: str
+    job_satisfaction: str
+    performance_rating: str
+    number_of_promotions: int
+    overtime: str
+    distance_from_home: int
+    education_level: str
+    marital_status: str
+    number_of_dependents: int
+    job_level: str
+    company_size: str
+    remote_work: str
+    leadership_opportunities: str
+    innovation_opportunities: str
+    company_reputation: str
+    employee_recognition: str
+    age_groups: str
+    age_before_working: int
+
 
 @app.post("/predict")
 def predict(data: EmployeeData):
     input_df = pd.DataFrame([data.dict()])
 
-    prediction = model.predict(input_df)
-    prediction_proba = model.predict_proba(input_df)[:, 1]  # probability for positive class
+    prediction = pipeline.predict(input_df)
+    prediction_proba = pipeline.predict_proba(input_df)[:, 0]
+    # Return the prediction and probability
+    # the propability is for the positive class (attrition = 1)
+    return {"prediction": int(prediction[0]), "probability": float(prediction_proba[0])}
 
-    return {
-        "prediction": int(prediction[0]),
-        "probability": float(prediction_proba[0])
-    }
 
 @app.get("/health")
 def health():
